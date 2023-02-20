3a4f2c6b08313c551f02a5094cef0fab1bb11af5
megumew <megumeww@gmail.com>
2023-02-20 01:17:11 -0500

    Beginnings of modpack detection implemented

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
+use std::fs;
 use std::io::{self, stdin};
-use std::path::Path;
-
-struct Data{
-
-}
+use std::path::{Path, PathBuf};
+struct Data {}
 
 fn main() {
     println!("\t\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\t\t| installing mods the easy way uwu |\n\t\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
@@ -14,23 +12,27 @@ fn main() {
     let app_data = paths.0;
     let vintage_story_path = paths.1;
 
-    println!("> Determined your %appdata% folder to be \"{}\"\n  If this is correct press enter... else close the program now!", app_data);
+    println!("> Determined your %appdata% folder to be {}\n  If this is correct press enter... else close the program now!", app_data);
 
+    wait_enter();
+
+    println!("> Your Vintage Story data is in {vintage_story_path}\n");
+
+    // Backup mod data if it already exists in program folder
+
+    // !TODO Query for local mode or server mode
+
+    // Check for a local modpack
+    check_mods_available().expect("Local modpack not installed correctly!");
+
+    wait_enter();
+}
+
+fn wait_enter() {
     //Dirty way to wait for enter to be pressed...
     stdin()
         .read_line(&mut String::new())
         .expect("Something went horribly wrong!");
-
-    println!("> Your Vintage Story data is in {vintage_story_path}\n");
-
-
-    // !TODO Query for local mode or server mode 
-
-    // Check that the modpack is read in data folder
-
-    
-
-    install_required_mods().expect("Error occured while instlaling the required mods!");
 }
 
 fn find_appata() -> (String, String) {
@@ -44,8 +46,38 @@ fn find_appata() -> (String, String) {
     (app_data, vintage_story_path)
 }
 
-fn install_required_mods() -> io::Result<()> {
-    let mods = std::fs::read_dir("data/mods")?
+fn check_mods_available() -> io::Result<()> {
+    let files = std::fs::read_dir("./")?
+        .map(|res| res.map(|e| e.path()))
+        .collect::<Result<Vec<_>, io::Error>>()?;
+
+    let mut modpack_found = false;
+    let mut modpacks: Vec<PathBuf> = Vec::new();
+    for file in files {
+        let path = file.join("mods");
+        if path.is_dir() {
+            modpacks.push(file);
+            modpack_found = true;
+        }
+    }
+
+    let modpack: PathBuf;
+    // !TODO NEED TO SET WAY TO SELECT BETWEEN MULTIPLE MODPACKS
+    if modpacks.len() > 1 {
+        println!("Multiple modpacks found please select one.");
+        modpack = modpacks.pop().unwrap();
+    } else {
+        modpack = modpacks.pop().unwrap();
+        println!("Modpack {} found!", modpack.display());
+    }
+
+    if !modpack_found {
+        println!("No modpack in same folder as program .exe");
+        wait_enter();
+        panic!("No modpack in same folder as program .exe");
+    }
+
+    let mods = std::fs::read_dir(modpack.join("mods"))?
         .map(|res| res.map(|e| e.path()))
         .collect::<Result<Vec<_>, io::Error>>()?;
 
